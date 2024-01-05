f5cdd7f1aaec288d388f34868b1ef6e15bea108a
pai-po <pai.po.sec@gmail.com>
2024-01-05 15:25:29 +0800

    Fix: 1. adjust advisories' request header

--- pysnowball/advisory.py
+++ pysnowball/advisory.py
@@ -4,18 +4,22 @@ from pysnowball import cons
 from pysnowball import api_ref
 from pysnowball import utls
 
+host = "xueqiu.com"
+
 
 def advisories(symbol, page=1, count=10):
     url = api_ref.advisories_latest+symbol
+    url = url + '&source=' + '公告'
     url = url + '&count='+str(count)
     url = url + '&page='+str(page)
 
-    return utls.fetch(url)
+    return utls.fetch(url, host)
 
 
 def news(symbol, page=1, count=10):
     url = api_ref.news_latest+symbol
+    url = url + '&source=' + '自选股新闻'
     url = url + '&count='+str(count)
     url = url + '&page='+str(page)
 
-    return utls.fetch(url)
+    return utls.fetch(url, host)
